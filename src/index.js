93837713dcaa7aee4c89512b65cb5ffc07e93b99
correoarg <164812858+correoarg@users.noreply.github.com>
2024-03-25 00:38:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import getFirestoreApp from './Firebase/Config'
 
+
+getFirestoreApp()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
